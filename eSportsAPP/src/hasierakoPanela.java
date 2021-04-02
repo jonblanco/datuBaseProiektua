394bbdb876b194a7bc7391c5656dfc63a6a153ce
jonblanco <jonblancoupvehu@gmail.com>
2021-04-02 16:22:24 +0200

    muchas cosas

--- eSportsAPP/src/hasierakoPanela.java
+++ eSportsAPP/src/hasierakoPanela.java
@@ -10,6 +10,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Window.Type;
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
 
 public class hasierakoPanela extends JFrame {
 
@@ -35,17 +38,21 @@ public class hasierakoPanela extends JFrame {
 	 * Create the frame.
 	 */
 	public hasierakoPanela() {
+		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Administratzailea");
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBackground(Color.DARK_GRAY);
-		btnNewButton.setFont(new Font("Eras Bold ITC", Font.PLAIN, 10));
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setBorder(null);
+		btnNewButton.setIcon(new ImageIcon(hasierakoPanela.class.getResource("/images/icons8_Maintenance_96px.png")));
+		btnNewButton.setForeground(new Color(255, 102, 0));
+		btnNewButton.setBackground(Color.WHITE);
+		btnNewButton.setFont(new Font("Microsoft Tai Le", Font.PLAIN, 10));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				login_Admin login= new login_Admin();
@@ -57,22 +64,38 @@ public class hasierakoPanela extends JFrame {
 		btnNewButton.setBounds(29, 75, 137, 94);
 		contentPane.add(btnNewButton);
 		
-		JButton btnErabiltzailea = new JButton("Erabiltzailea");
-		btnErabiltzailea.setForeground(Color.WHITE);
-		btnErabiltzailea.setBackground(Color.DARK_GRAY);
-		btnErabiltzailea.setFont(new Font("Eras Bold ITC", Font.PLAIN, 11));
+		JButton btnErabiltzailea = new JButton("");
+		btnErabiltzailea.setBorder(null);
+		btnErabiltzailea.setMaximumSize(new Dimension(50, 20));
+		btnErabiltzailea.setIcon(new ImageIcon(hasierakoPanela.class.getResource("/images/icons8_User_96px_2.png")));
+		btnErabiltzailea.setForeground(new Color(255, 102, 0));
+		btnErabiltzailea.setBackground(Color.WHITE);
+		btnErabiltzailea.setFont(new Font("Microsoft Tai Le", Font.PLAIN, 11));
 		btnErabiltzailea.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-		btnErabiltzailea.setBounds(266, 75, 137, 94);
+		btnErabiltzailea.setBounds(271, 75, 137, 94);
 		contentPane.add(btnErabiltzailea);
 		
 		JLabel lblNewLabel = new JLabel("Zer zara?");
-		lblNewLabel.setFont(new Font("Eras Bold ITC", Font.BOLD | Font.ITALIC, 17));
+		lblNewLabel.setForeground(new Color(255, 102, 0));
+		lblNewLabel.setFont(new Font("Microsoft Tai Le", Font.BOLD, 18));
 		lblNewLabel.setBounds(173, 35, 166, 29);
 		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Administratzailea");
+		lblNewLabel_1.setForeground(new Color(255, 102, 0));
+		lblNewLabel_1.setFont(new Font("Microsoft Tai Le", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(39, 180, 120, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Erabiltzailea");
+		lblNewLabel_1_1.setForeground(new Color(255, 102, 0));
+		lblNewLabel_1_1.setFont(new Font("Microsoft Tai Le", Font.PLAIN, 16));
+		lblNewLabel_1_1.setBounds(296, 180, 128, 14);
+		contentPane.add(lblNewLabel_1_1);
 		setLocationRelativeTo(null);
 	}
 }
--- eSportsAPP/src/login_Admin.java
+++ eSportsAPP/src/login_Admin.java
@@ -15,11 +15,13 @@ import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JSeparator;
 //
 public class login_Admin extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
+	private JTextField txtIdatziHemenZure;
 	private JPasswordField passwordField;
 
 	/**
@@ -42,83 +44,117 @@ public class login_Admin extends JDialog {
 		getContentPane().setBackground(Color.DARK_GRAY);
 		setTitle("Hasiera panela");
 		
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 342, 485);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBackground(Color.DARK_GRAY);
+		contentPanel.setBackground(Color.WHITE);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		setLocationRelativeTo(null);
 		{
-			JLabel lblNewLabel = new JLabel("Izena");
-			lblNewLabel.setForeground(Color.WHITE);
-			lblNewLabel.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 15));
-			lblNewLabel.setBounds(82, 89, 107, 31);
+			JLabel lblNewLabel = new JLabel("Izena:");
+			lblNewLabel.setForeground(new Color(255, 102, 0));
+			lblNewLabel.setFont(new Font("Microsoft Tai Le", Font.PLAIN, 18));
+			lblNewLabel.setBounds(72, 131, 107, 31);
 			contentPanel.add(lblNewLabel);
 		}
 		{
-			textField = new JTextField();
-			textField.setBounds(151, 89, 213, 31);
-			contentPanel.add(textField);
-			textField.setColumns(10);
+			txtIdatziHemenZure = new JTextField();
+			txtIdatziHemenZure.setForeground(Color.LIGHT_GRAY);
+			txtIdatziHemenZure.setBorder(null);
+			txtIdatziHemenZure.setToolTipText("");
+			txtIdatziHemenZure.setBounds(72, 163, 175, 31);
+			contentPanel.add(txtIdatziHemenZure);
+			txtIdatziHemenZure.setColumns(10);
 		}
 		{
-			JLabel lblNewLabel_1 = new JLabel("Pasahitza");
-			lblNewLabel_1.setForeground(Color.WHITE);
-			lblNewLabel_1.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 15));
-			lblNewLabel_1.setBounds(82, 131, 87, 37);
+			JLabel lblNewLabel_1 = new JLabel("Pasahitza:");
+			lblNewLabel_1.setForeground(new Color(255, 102, 0));
+			lblNewLabel_1.setFont(new Font("Microsoft Tai Le", Font.PLAIN, 18));
+			lblNewLabel_1.setBounds(72, 221, 87, 37);
 			contentPanel.add(lblNewLabel_1);
 		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setBackground(Color.DARK_GRAY);
-			buttonPane.setBounds(5, 194, 421, 63);
-			contentPanel.add(buttonPane);
-			buttonPane.setLayout(null);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						String pasahitza= "admin";
-						if (passwordField.getText().equals(pasahitza)) {
-						// lab_GUI ktrl_panela= new lab_GUI();
-						 setVisible(false);
-						 
-					//	 ktrl_panela.main(null);
-						}else {
-							JOptionPane.showMessageDialog(null, "Pasahitza txarto dago!");
-							passwordField.setText(null);
-							passwordField.requestFocus();
-						}
-					}
-				});
-				okButton.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 10));
-				okButton.setBounds(154, 5, 47, 23);
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						textField.setText(null);
-						passwordField.setText(null);
-					}
-				});
-				cancelButton.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 11));
-				cancelButton.setBounds(211, 5, 65, 23);
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
 		contentPanel.add(getPasswordField());
+		{
+			JLabel label = new JLabel("");
+			label.setIcon(new ImageIcon(login_Admin.class.getResource("/images/icons8_User_96px_2.png")));
+			label.setBounds(110, 28, 96, 85);
+			contentPanel.add(label);
+		}
+		{
+			JButton okButton = new JButton("");
+			okButton.setBorder(null);
+			okButton.setBackground(Color.WHITE);
+			okButton.setBounds(110, 326, 107, 63);
+			contentPanel.add(okButton);
+			okButton.setIcon(new ImageIcon(login_Admin.class.getResource("/images/Enter_ON.png")));
+			okButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String pasahitza= "admin";
+					if (passwordField.getText().equals(pasahitza)) {
+					// lab_GUI ktrl_panela= new lab_GUI();
+					 setVisible(false);
+					 
+				//	 ktrl_panela.main(null);
+					}else {
+						JOptionPane.showMessageDialog(null, "Pasahitza txarto dago!");
+						passwordField.setText(null);
+						passwordField.requestFocus();
+					}
+				}
+			});
+			okButton.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 10));
+			okButton.setActionCommand("OK");
+			getRootPane().setDefaultButton(okButton);
+		}
+		{
+			JButton cancelButton = new JButton("");
+			cancelButton.setBorder(null);
+			cancelButton.setBounds(10, 11, 44, 39);
+			contentPanel.add(cancelButton);
+			cancelButton.setBackground(Color.WHITE);
+			cancelButton.setForeground(new Color(0, 0, 0));
+			cancelButton.setIcon(new ImageIcon(login_Admin.class.getResource("/images/icons8_Multiply_32px.png")));
+			cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					txtIdatziHemenZure.setText(null);
+					passwordField.setText(null);
+				}
+			});
+			cancelButton.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 11));
+			cancelButton.setActionCommand("Cancel");
+		}
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(Color.BLACK);
+		separator.setBounds(72, 194, 175, 16);
+		contentPanel.add(separator);
+		{
+			JSeparator separator_1 = new JSeparator();
+			separator_1.setBackground(Color.BLACK);
+			separator_1.setBounds(72, 288, 175, 16);
+			contentPanel.add(separator_1);
+		}
+		{
+			JLabel lblImglog = new JLabel("");
+			lblImglog.setIcon(new ImageIcon(login_Admin.class.getResource("/images/icons8_customer_32px_1.png")));
+			lblImglog.setBounds(30, 163, 32, 39);
+			contentPanel.add(lblImglog);
+		}
+		{
+			JLabel lblPswd = new JLabel("");
+			lblPswd.setIcon(new ImageIcon(login_Admin.class.getResource("/images/icons8_Key_32px.png")));
+			lblPswd.setBounds(30, 256, 32, 41);
+			contentPanel.add(lblPswd);
+		}
 	}
 	private JPasswordField getPasswordField() {
 		if (passwordField == null) {
 			passwordField = new JPasswordField();
+			passwordField.setForeground(Color.LIGHT_GRAY);
+			passwordField.setBorder(null);
 			passwordField.setEchoChar('*');
-			passwordField.setBounds(151, 131, 213, 29);
+			passwordField.setBounds(72, 256, 175, 29);
 		}
 		return passwordField;
 	}
